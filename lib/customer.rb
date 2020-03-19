1c60e36527a3def9a0f388cdd3f1292111776f30
Tim Donoghue <tmd112593@gmail.com>
2020-03-19 19:48:20 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -9,13 +9,10 @@ class Customer
     @@all << self 
   end
   
+  
+  
   def self.all
     @@all
   end 
-  
-  def new_meal(waiter, total, tip=0)
-    Meal.new(waiter, self, total, tip)
-  end 
-  
   
 end 
