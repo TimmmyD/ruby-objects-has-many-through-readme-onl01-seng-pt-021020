fa5a477f168de788f9e9209280fd239aa4179753
Tim Donoghue <tmd112593@gmail.com>
2020-03-19 20:09:38 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -32,3 +32,14 @@ class Waiter
 
 end 
 
+jason = Waiter.new("Jason", 4)
+lisa = Customer.new("Lisa", 24)
+tim = Customer.new("Tim", 35)
+terrance = Customer.new("Terrance", 27)
+ 
+terrance.new_meal(jason, 50, 3)
+lisa.new_meal(jason, 40, 10)
+tim.new_meal(jason, 45, 8)
+ 
+jason.best_tipper 
+jason.best_tipper.name 
