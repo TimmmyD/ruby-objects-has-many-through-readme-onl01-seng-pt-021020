5b87737d98a48ca83a7506d2b50eba920345ce77
Tim Donoghue <tmd112593@gmail.com>
2020-03-19 19:55:22 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -13,17 +13,10 @@ class Customer
     Meal.new(waiter, self, total, tip)
   end 
   
-  sam = Customer.new("Sam", 27)
-  pat = Waiter.new("Pat", 2)
-  alex = Waiter.new("Alex", 5)
-  
-  sam.new_meal(pat, 50, 10)
-  sam.new_meal(alex, 20, 3)
-  pat.new_meal(sam, 30, 5)
-  
-  
-  def self.all
+ def self.all
     @@all
   end 
   
 end 
+
+
